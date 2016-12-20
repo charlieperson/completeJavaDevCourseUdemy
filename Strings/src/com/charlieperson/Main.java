c868b490ed45f5e8b38aafc538f86b2140be7174
charlieperson <charlie.person@hyperisland.se>
2016-12-19 23:44:53 -0600

    Adds virtual dom problem- not yet solved

--- Strings/src/com/charlieperson/Main.java
+++ Strings/src/com/charlieperson/Main.java
@@ -3,8 +3,10 @@ package com.charlieperson;
 public class Main {
 
     public static void main(String[] args) {
-	    String ellie = "stinker";
-
+	    double myVar = 20d;
+        double secondVar = 81d;
+        if (((myVar + secondVar) * 25) % 40 <= 20)
+            System.out.println("Over the limit: " + ((myVar + secondVar) * 25) % 40);
 
     }
 }
