e3e8989a6df0fe283ad283872c9b44846a7eaac7
charlieperson <charlie.person@hyperisland.se>
2016-12-21 16:44:32 -0600

    Completes switch state challenge

--- switch/src/com/charlieperson/Main.java
+++ switch/src/com/charlieperson/Main.java
@@ -18,5 +18,34 @@ public class Main {
                 System.out.println("The number is something other than a nine or a two");
                 break;
         }
+
+        char switchChar = 'D';
+
+        switch(switchChar) {
+            case 'A':
+                System.out.println("BIG A");
+                break;
+
+            case 'B':
+                System.out.println("BIG B");
+                break;
+
+
+            case 'C':
+                System.out.println("BIG C");
+                break;
+
+            case 'D':
+                System.out.println("BIG D");
+                break;
+
+            case 'E':
+                System.out.println("BIG E");
+                break;
+
+            default:
+                System.out.println("SOMETHIN' ELSE");
+                break;
+        }
     }
 }
