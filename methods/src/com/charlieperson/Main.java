8648823f2ebf09d71a870301de8ef8f92f272b29
charlieperson <charlie.person@hyperisland.se>
2016-12-21 13:39:46 -0600

    Adds methods exercise code

---
+++ methods/src/com/charlieperson/Main.java
@@ -0,0 +1,17 @@
+package com.charlieperson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Your final score was " + calculateScore(true, 800, 5, 100));
+        System.out.println("Your final score was " + calculateScore(true, 10000, 8, 200));
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            return finalScore;
+        } else return 0;
+    }
+
+}
