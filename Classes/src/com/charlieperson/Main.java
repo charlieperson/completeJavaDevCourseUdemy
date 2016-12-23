8e9806b92f3794a17a7bc37a1f0ad900b330030d
charlieperson <charlie.person@hyperisland.se>
2016-12-22 23:49:24 -0600

    Adds exercise code for classes part one

---
+++ Classes/src/com/charlieperson/Main.java
@@ -0,0 +1,14 @@
+package com.charlieperson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car();
+
+        porsche.setColor("Red");
+        porsche.setDoors(4);
+        porsche.setEngine("V8");
+        porsche.setModel("911 Turbo");
+        porsche.setWheels(4);
+    }
+}
