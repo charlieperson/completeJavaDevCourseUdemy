bff0b69a4befe6b9c4dc506a56b0bff7b507bea4
charlieperson <charlie.person@hyperisland.se>
2016-12-24 16:52:03 -0600

    Improves bank account code by implementing the use of this() for default parameters by overloading the constructors for the Account Class

--- ClassesChallenge/src/com/charlieperson/Account.java
+++ ClassesChallenge/src/com/charlieperson/Account.java
@@ -7,6 +7,18 @@ public class Account {
     private String email;
     private String phoneNumber;
 
+    public Account() {
+        this(0, 0.0, "Default Name", "Default email", "000");
+    }
+
+    public Account(int accountNumber, double balance, String customerName, String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
     public void deposit(double amount) {
         System.out.println("$" + amount + " added to balance of $" + this.balance);
         this.balance += amount;
@@ -23,42 +35,43 @@ public class Account {
                     "You only have $" + this.balance +
                     " but you tried to take out: $" + amount);
         }
+
     }
 
     public int getAccountNumber() {
-        return this.accountNumber;
-    }
-
-    public double getBalance() {
-        return this.balance;
-    }
-
-    public String getCustomerName() {
-        return this.customerName;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
+        return accountNumber;
     }
 
     public void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
     }
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
--- ClassesChallenge/src/com/charlieperson/Main.java
+++ ClassesChallenge/src/com/charlieperson/Main.java
@@ -7,5 +7,8 @@ public class Main {
         chucksAccount.deposit(44.34);
         chucksAccount.withdrawal(50);
         chucksAccount.withdrawal(10);
+        Account newAccount = new Account(543432, 77.0, "Dirty Harry", "harry@examp.com", "512-789-5432");
+        System.out.println(newAccount.getAccountNumber());
+
     }
 }
