987efede67207db421e992089c1eaf6195c3feb7
charlieperson <charlie.person@hyperisland.se>
2016-12-25 00:08:05 -0600

    Completes VipCustomer constructor challenge

--- ClassesChallenge/src/com/charlieperson/Main.java
+++ ClassesChallenge/src/com/charlieperson/Main.java
@@ -3,15 +3,29 @@ package com.charlieperson;
 public class Main {
 
     public static void main(String[] args) {
-	    Account chucksAccount = new Account();
-        Account onlyThree = new Account("Fred", "Fred@fred.com", "412-3212-3424");
-        System.out.println(onlyThree.getBalance());
-        System.out.println(chucksAccount.getAccountNumber());
-        System.out.println(chucksAccount.getEmail());
-        chucksAccount.deposit(44.34);
-        chucksAccount.withdrawal(50);
-        chucksAccount.withdrawal(10);
-        Account newAccount = new Account(543432, 77.0, "Dirty Harry", "harry@examp.com", "512-789-5432");
-        System.out.println(newAccount.getAccountNumber());
+//	    Account chucksAccount = new Account();
+//        Account onlyThree = new Account("Fred", "Fred@fred.com", "412-3212-3424");
+//        System.out.println(onlyThree.getBalance());
+//        System.out.println(chucksAccount.getAccountNumber());
+//        System.out.println(chucksAccount.getEmail());
+//        chucksAccount.deposit(44.34);
+//        chucksAccount.withdrawal(50);
+//        chucksAccount.withdrawal(10);
+//        Account newAccount = new Account(543432, 77.0, "Dirty Harry", "harry@examp.com", "512-789-5432");
+//        System.out.println(newAccount.getAccountNumber());
+
+        VipCustomer mrBigShot = new VipCustomer();
+        System.out.println(mrBigShot.getCreditLimit());
+        System.out.println(mrBigShot.getEmailAddress());
+        System.out.println("-------------------------");
+        VipCustomer coupleOfParamsGuy = new VipCustomer("Coka-Cola", 100000);
+        System.out.println(coupleOfParamsGuy.getEmailAddress());
+        System.out.println(coupleOfParamsGuy.getName());
+        System.out.println(coupleOfParamsGuy.getCreditLimit());
+        System.out.println("-------------------------");
+        VipCustomer allOfEm = new VipCustomer("This guy", 234, "anotha@email.com");
+        System.out.println(allOfEm.getCreditLimit());
+        System.out.println(allOfEm.getEmailAddress());
+        System.out.println(allOfEm.getName());
     }
 }
--- ClassesChallenge/src/com/charlieperson/VipCustomer.java
+++ ClassesChallenge/src/com/charlieperson/VipCustomer.java
@@ -1,5 +1,33 @@
 package com.charlieperson;
 
 public class VipCustomer {
+    private String name;
+    private int creditLimit;
+    private String emailAddress;
 
+    VipCustomer() {
+        this("Franky Examp", 45, "franky@examp.com");
+    }
+
+    VipCustomer(String name, int creditLimit) {
+        this(name, creditLimit, "default@email.com");
+    }
+
+    VipCustomer(String name, int creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
