a513fa3011dfee17abd3c99417578720c6289bcf
charlieperson <charlie.person@hyperisland.se>
2016-12-25 00:39:02 -0600

    Completes inheritance exercise

---
+++ inheritance/src/com/charlieperson/Main.java
@@ -0,0 +1,10 @@
+package com.charlieperson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal georgeTheElephant = new Animal("buster", 1, 1, 10, 100);
+        Dog dog = new Dog("YorkShire", 30, 65, 2, 4, 1, 40, "brown");
+        dog.eat();
+    }
+}
