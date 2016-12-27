306f5d7fd0063d7eb26296b6d23d475735046c1f
charlieperson <charlie.person@hyperisland.se>
2016-12-26 23:46:43 -0600

    Completes inheritance challenge solution code

---
+++ InheritanceChallengeSolution/src/com/charlieperson/Main.java
@@ -0,0 +1,12 @@
+package com.charlieperson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Outlander outlander = new Outlander(12);
+        outlander.accelerate(30);
+        outlander.steer(60);
+        outlander.accelerate(50);
+    }
+
+}
