ed357cb733d04bdd45b89f4d27950302e9eb39bb
charlieperson <charlie.person@hyperisland.se>
2016-12-27 09:25:09 -0600

    Completes house composition challenge solution code

---
+++ HouseCompositionSolution/src/com/charlieperson/Ceiling.java
@@ -0,0 +1,22 @@
+package com.charlieperson;
+
+/**
+ * Created by charlieperson on 12/27/16.
+ */
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
---
+++ HouseCompositionSolution/src/com/charlieperson/Wall.java
@@ -0,0 +1,16 @@
+package com.charlieperson;
+
+/**
+ * Created by charlieperson on 12/27/16.
+ */
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
