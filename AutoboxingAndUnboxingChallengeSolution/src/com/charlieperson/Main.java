8e0fa320b6c3df4dfd21bb4f6a2792a8c4fe55f8
Charlie Person <charlie.person@hyperisland.se>
2017-01-18 20:42:40 -0500

    Adds testing code to the Main class

--- AutoboxingAndUnboxingChallengeSolution/src/com/charlieperson/Main.java
+++ AutoboxingAndUnboxingChallengeSolution/src/com/charlieperson/Main.java
@@ -9,5 +9,19 @@ public class Main {
         capitalOne.addCustomerTransaction("Reggie Miller", "Chuck P", 500);
         capitalOne.addCustomer("Reggie Miller", "Jessica", 300);
         capitalOne.listCustomers("Reggie Miller");
+
+        capitalOne.addBranch("Melbourne");
+        if(!capitalOne.addBranch("Melbourne")) {
+            System.out.println("branch is already there boss");
+        }
+
+        if(!capitalOne.addCustomer("Reggie Miller", "Chuck P", 100)) {
+            System.out.println("customer already exists boss");
+        }
+
+        if(!capitalOne.addCustomerTransaction("Reggie Miller", "some dude..", 24423)) {
+            System.out.println("that dude aint in this branch boss");
+        }
+
     }
 }
