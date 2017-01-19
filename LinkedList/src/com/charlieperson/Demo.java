2d7ce1310806058dac8dd5cce63331f262abdb3c
Charlie Person <charlie.person@hyperisland.se>
2017-01-19 12:26:48 -0500

    Completes LinkedList exercise code functionality

--- LinkedList/src/com/charlieperson/Demo.java
+++ LinkedList/src/com/charlieperson/Demo.java
@@ -3,22 +3,19 @@ package com.charlieperson;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Whatever");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Outback Steakhouse");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Perth");
+        addInOrder(placesToVisit, "Stockholm");
+        addInOrder(placesToVisit, "Miami");
+        addInOrder(placesToVisit, "Austin");
+        addInOrder(placesToVisit, "Gainsville");
+        addInOrder(placesToVisit, "Chicago");
+        addInOrder(placesToVisit, "Chicago");
         printList(placesToVisit);
-
-        placesToVisit.add(1, "Alice Springs");
-        printList(placesToVisit);
-
-        placesToVisit.remove(4);
+        visit(placesToVisit);
     }
 
     private static void printList(LinkedList<String> linkedList) {
@@ -46,6 +43,64 @@ public class Demo {
         }
 
         stringListIterator.add(newCity);
+        return true;
+    }
+
+    private static void visit(LinkedList<String> cities) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+        displayOptions();
+        System.out.println("You are now in " + listIterator.next());
+        while(!quit) {
+            System.out.println("What would you like to do?");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    displayOptions();
+                    break;
+                case 2:
+                    if(!goingForward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("You are now in " + listIterator.next());
+                    } else {
+                        System.out.println("This is the end of the list...");
+                    }
+                    break;
+                case 3:
+                    if(goingForward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("You are now in " + listIterator.previous());
+                    } else {
+                        System.out.println("This is the beginning of the list...");
+                    }
+                    break;
+                case 4:
+                    quit = true;
+                    break;
+            }
+        }
+
+    }
+
+    private static void displayOptions() {
+        System.out.println("1- print options");
+        System.out.println("2- go forward");
+        System.out.println("3- go back");
+        System.out.println("quit");
     }
 
 
